b1c8b43d01b39ad064a9d64ab3bafd96c823e7e8
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-29 10:38:11 -0500

    correcting spelling mistake

--- module/main.tf
+++ module/main.tf
@@ -17,16 +17,16 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "aws_route53_record" "record" {
-  name    = var.tool_name
-  type    = "A"
-  zone_id = var.zone_id
-  records = [aws_instance.instance.public_ip]
-  ttl = 30
-}
+# resource "aws_route53_record" "record" {
+#   name    = var.tool_name
+#   type    = "A"
+#   zone_id = var.zone_id
+#   records = [aws_instance.instance.public_ip]
+#   ttl = 30
+# }
 
 resource "aws_route53_record" "record" {
-  name    = "${var.component}-${var.env}"
+  name    = "${var.tool_name}-internal"
   type    = "A"
   zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
